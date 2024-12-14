2ca8ab0abb601183acd22720f0129f381b63c107
jacobjelen <jacobjelen@gmail.com>
2024-12-14 16:52:36 +0100

    fixing build errors

--- packages/nextjs/app/artifacts/@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol/AggregatorV3Interface.d.ts
+++ packages/nextjs/app/artifacts/@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol/AggregatorV3Interface.d.ts
@@ -4,130 +4,130 @@
 // eslint-disable
 
 import type { Address } from "viem";
-import type { GetContractReturnType } from "@nomicfoundation/hardhat-viem/types";
+import type { GetContractReturnType, GetTransactionReturnType, DeployContractConfig, SendDeploymentTransactionConfig, GetContractAtConfig } from "@nomicfoundation/hardhat-viem/types";
 import "@nomicfoundation/hardhat-viem/types";
 
 export interface AggregatorV3Interface$Type {
-  "_format": "hh-sol-artifact-1",
-  "contractName": "AggregatorV3Interface",
-  "sourceName": "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol",
+  "_format": "hh-sol-artifact-1";
+  "contractName": "AggregatorV3Interface";
+  "sourceName": "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
   "abi": [
     {
-      "inputs": [],
-      "name": "decimals",
+      "inputs": [];
+      "name": "decimals";
       "outputs": [
         {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
+          "internalType": "uint8";
+          "name": "";
+          "type": "uint8";
         }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+      ];
+      "stateMutability": "view";
+      "type": "function";
     },
     {
-      "inputs": [],
-      "name": "description",
+      "inputs": [];
+      "name": "description";
       "outputs": [
         {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
+          "internalType": "string";
+          "name": "";
+          "type": "string";
         }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+      ];
+      "stateMutability": "view";
+      "type": "function";
     },
     {
       "inputs": [
         {
-          "internalType": "uint80",
-          "name": "_roundId",
-          "type": "uint80"
+          "internalType": "uint80";
+          "name": "_roundId";
+          "type": "uint80";
         }
-      ],
-      "name": "getRoundData",
+      ];
+      "name": "getRoundData";
       "outputs": [
         {
-          "internalType": "uint80",
-          "name": "roundId",
-          "type": "uint80"
+          "internalType": "uint80";
+          "name": "roundId";
+          "type": "uint80";
         },
         {
-          "internalType": "int256",
-          "name": "answer",
-          "type": "int256"
+          "internalType": "int256";
+          "name": "answer";
+          "type": "int256";
         },
         {
-          "internalType": "uint256",
-          "name": "startedAt",
-          "type": "uint256"
+          "internalType": "uint256";
+          "name": "startedAt";
+          "type": "uint256";
         },
         {
-          "internalType": "uint256",
-          "name": "updatedAt",
-          "type": "uint256"
+          "internalType": "uint256";
+          "name": "updatedAt";
+          "type": "uint256";
         },
         {
-          "internalType": "uint80",
-          "name": "answeredInRound",
-          "type": "uint80"
+          "internalType": "uint80";
+          "name": "answeredInRound";
+          "type": "uint80";
         }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+      ];
+      "stateMutability": "view";
+      "type": "function";
     },
     {
-      "inputs": [],
-      "name": "latestRoundData",
+      "inputs": [];
+      "name": "latestRoundData";
       "outputs": [
         {
-          "internalType": "uint80",
-          "name": "roundId",
-          "type": "uint80"
+          "internalType": "uint80";
+          "name": "roundId";
+          "type": "uint80";
         },
         {
-          "internalType": "int256",
-          "name": "answer",
-          "type": "int256"
+          "internalType": "int256";
+          "name": "answer";
+          "type": "int256";
         },
         {
-          "internalType": "uint256",
-          "name": "startedAt",
-          "type": "uint256"
+          "internalType": "uint256";
+          "name": "startedAt";
+          "type": "uint256";
         },
         {
-          "internalType": "uint256",
-          "name": "updatedAt",
-          "type": "uint256"
+          "internalType": "uint256";
+          "name": "updatedAt";
+          "type": "uint256";
         },
         {
-          "internalType": "uint80",
-          "name": "answeredInRound",
-          "type": "uint80"
+          "internalType": "uint80";
+          "name": "answeredInRound";
+          "type": "uint80";
         }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+      ];
+      "stateMutability": "view";
+      "type": "function";
     },
     {
-      "inputs": [],
-      "name": "version",
+      "inputs": [];
+      "name": "version";
       "outputs": [
         {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
+          "internalType": "uint256";
+          "name": "";
+          "type": "uint256";
         }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+      ];
+      "stateMutability": "view";
+      "type": "function";
     }
-  ],
-  "bytecode": "0x",
-  "deployedBytecode": "0x",
-  "linkReferences": {},
-  "deployedLinkReferences": {}
+  ];
+  "bytecode": "0x";
+  "deployedBytecode": "0x";
+  "linkReferences": Record<string, never>;
+  "deployedLinkReferences": Record<string, never>;
 }
 
 declare module "@nomicfoundation/hardhat-viem/types" {
