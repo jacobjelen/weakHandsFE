e431709770c4501b4f066ab47d79bdbe03c94127
jacobjelen <jacobjelen@gmail.com>
2024-12-15 17:13:28 +0100

    card bg test

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -60,6 +60,10 @@ module.exports = {
     extend: {
       boxShadow: { center: "0 0 12px -2px rgb(0 0 0 / 0.05)" },
       animation: { "pulse-fast": "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite" },
+      colors: {
+        card: 'var(--background)',
+        'card-foreground': 'var(--foreground)',
+      },
     },
   },
 };
